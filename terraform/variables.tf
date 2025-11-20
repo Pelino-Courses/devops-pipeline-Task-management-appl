4f974cf23c501ddd25f2c721cbfe74f605ea4d1d
Alain SHEMA <alainshema@gmail.com>
2025-11-20 22:15:31 +0200

    Fix Terraform: Change region to East US 2 to avoid policy restriction

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "East US"
+  default     = "East US 2"
 }
 
 variable "vm_size" {
